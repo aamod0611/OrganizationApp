5cc7cf9555880a62b1a31625a9d8c91feb3ee73f
aamod0611 <aamod0611@gmail.com>
2017-06-09 15:49:08 +0530

    popta commit kar srake

--- main/forms.py
+++ main/forms.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.models import User
 from django import forms
-from . models import TeamMembers
-
+from . models import TeamMembers,Teams
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput)
 
